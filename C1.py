0c2b0a51f8ca82c3e8cb33f8f170161c0af8b687
Vitaliy5 <47832017+Vitaliy5@users.noreply.github.com>
2019-06-09 19:35:08 +0300

    All done

--- C1.py
+++ C1.py
@@ -5,6 +5,16 @@ def printTimeStamp(name):
   print('\nАвтор програми: ' + name)
   print('Час компіляції: ' + str(datetime.datetime.now()))
 
+def precedence(operator):
+    if operator in ["-","+"]:
+        return 1
+    if operator in ["*","/"]:
+        return 2
+    if operator == "^":
+        return 3
+    else:
+        return -1
+
 operators = []
 postfixs =[]
 expression = input("Інфіксований вираз: ")
@@ -20,19 +30,19 @@ for i in range(len(tokens)-1):
                 break
     except IndexError:
         pass
-print(tokens)
+
 for token in tokens:
     if token.isdigit():
         postfixs.append(token)
-    if token in "*/+-()":
-        while operators != [] and operators[-1] != "(" and precedence(token) < precedence(operators[-1]):
-            print("add",operators[-1], token)
+    if token in "*/+-^":
+        while operators != [] and operators[-1] != "(" and  precedence(token) < precedence(operators[-1]):
+            print("2: add",operators[-1],"by tok:", token)
             postfixs.append(operators[-1])
             operators.remove(operators[-1])
         operators.append(token)
     if token == "(":
         operators.append(token)
-    if token == "(":
+    if token == ")":
         while operators[-1] != "(":
             postfixs.append(operators[-1])
             operators.remove(operators[-1])
@@ -42,5 +52,5 @@ while operators != []:
     postfixs.append(operators[-1])
     operators.remove(operators[-1])
 
-print(postfixs)
+print(" ".join(postfixs))
 printTimeStamp("Віталій Дудник")
