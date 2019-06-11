7beab0acf6b745ec6927d6e5f0b437b5c9d29b6a
Vitaliy5 <47832017+Vitaliy5@users.noreply.github.com>
2019-06-11 20:24:33 +0300

    new

--- C2.py
+++ C2.py
@@ -5,8 +5,15 @@ def printTimeStamp(name):
   print('\nАвтор програми: ' + name)
   print('Час компіляції: ' + str(datetime.datetime.now()))
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 values = []
-expression = input("Інфіксований вираз: ")
+expression = input("Постфіксований вираз: ")
 tokens = [i for i in expression]
 for i in range(len(tokens)-1):
     try :
@@ -16,21 +23,23 @@ for i in range(len(tokens)-1):
             if tokens[i].isdigit() and tokens[i+1].isdigit():
                 tokens[i] = tokens[i] + tokens[i+1]
                 tokens.remove(tokens[i+1])
+            if tokens[i] in "+-" and tokens[i+1].isdigit():
+                tokens[i] = tokens[i] + tokens[i + 1]
+                tokens.remove(tokens[i + 1])
             else:
                 break
     except IndexError:
         pass
-print(tokens)
+
 for token in tokens:
-    if token.isdigit():
+    if is_number(token):
         values.append(int(token))
     else:
         right = values[-1]
         values.remove(values[-1])
         left = values[-1]
         values.remove(values[-1])
-        result = eval("{} {} {}".format(right, token ,left))
+        result = eval("{} {} {}".format(left, token ,right))
         values.append(result)
-print(values)
-
+print(values[0])
 printTimeStamp("Віталій Дудник")
