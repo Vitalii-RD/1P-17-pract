56704431e5aa3695c5cd29919d3cd3752372b03e
korobovdk <51205523+korobovdk@users.noreply.github.com>
2019-06-11 23:18:56 +0300

    Add files via upload

--- A3.py
+++ A3.py
@@ -5,15 +5,18 @@ def printTimeStamp(name):
   print('\nАвтор програми: ' + name)
   print('Час компіляції: ' + str(datetime.datetime.now()))
 
-words = set()
-while True:
-    word = input("Введіть слово:")
-    if word == "":
-        break
-        
+import random
+bukwa = (input("Буква:")).lower()
+if bukwa in "aouei":
+    print("Голосна")
+elif bukwa == "y":
+    rand = random.randint(0,1)
+    if rand == 1:
+        print("Голосна")
     else:
-        words.add(word)
-for i in words:
-    print(i)
+        print("Приголосна")
+else:
+    print("Приголосна")
 
 printTimeStamp("Віталій Дудник")
+
